d8f3711230a9244db88e1d8afdd8ba45525fe357
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-04-12 17:41:38 +0200

    FEAT html forelse and card

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,25 @@
 @extends('layouts.app')
 @section('content')
 
-
 <div class="container py-5">
-    <h1>ciao</h1>
+    <div class="row gy-3 justify-content-center">
+        @forelse ($trains as $train)
+        <div class="card col-5 m-3 text-bg-primary">
+            <div class="card-body">
+                <h5 class="card-title">Compagnia: {{$train->company}}</h5>
+                <h5 class="card-title">Citta' di partenza: {{$train->departure_station}}</h5>
+                <h5 class="card-title">Citta' d'arrivo: {{$train->arrival_station}}</h5>
+                <h3 class="card-title">Orari</h3>
+                <p class="card-text">Partenza: {{$train->departure_time}}</p>
+                <p class="card-text">Arrivo: {{$train->arrival_time}}</p>
+                <p class="card-text">Numero carrozze: {{$train->number_of_coaches}}</p>
+                <p class="card-text"><small class="text-body-secondary">In orario: {{$train->on_schedule}}</small></p>
+            </div>
+        </div>
+        @empty
+
+
+        @endforelse
+    </div>
 </div>
 @endsection
